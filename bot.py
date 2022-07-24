8fc6fb192d3606dfc0193675e2b68d0f31146fbb
Fernando Zuniga <fernando.zuniga@foodservice.com.gt>
2022-07-23 23:49:55 -0600

    REESTABLECER

--- bot.py
+++ bot.py
@@ -1,136 +1,65 @@
-# .\venv\Scripts\activate
 import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
-from telegram.ext import (
-    Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
-)
-
-from mail import *
-import requests
-
-
-
-# Token provided by @ BotFather
-TOKEN = '5530425914:AAEUWQHLPVtM2ksZ6KQS3V8NmGKX_N7-6Sk' 
-# Your Heroku App Page 
-APP_NAME = 'https://sergesa-bot.herokuapp.com/'
-# Port
 PORT = int(os.environ.get('PORT', '8443'))
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
-logger = logging.getLogger(__name__)
 
-# Functions
+logger = logging.getLogger(__name__)
+TOKEN = '5530425914:AAEUWQHLPVtM2ksZ6KQS3V8NmGKX_N7-6Sk'
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('''Hello ! I am your shipment tracking bot, please find below commands to interract with me. \n /start_tracking: start tracking process''')
-<<<<<<< HEAD
-# Location Tracking
-LOC, DN, PHOTO = range(3)
+    update.message.reply_text('Hi!')
 
-def start_tracking(update: Update, context: CallbackContext):
-    user = update.message.from_user
-    logger.info("Driver: %s", user.first_name)
-    update.message.reply_text(
-        'Please share your current GPS location.',
-        reply_markup = ReplyKeyboardRemove(),
-    )
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
 
-    return LOC
-
-    def photo(update: Update, context: CallbackContext):
-        user = update.message.from_user
-        photo_file = update.message.photo[-1].get_file()
-        # photo_file.download('user_photo.jpg')
-
-    # Get link to get file_path
-    link1 = "https://api.telegram.org/bot{}/getfile?file_id={}".format(TOKEN, photo_file.file_id)
-    r = requests.get(link1)
-    file_path = r.json()["result"]["file_path"]
-
-    # Link to download file
-    link2 = "https://api.telegram.org/file/bot{}/{}".format(TOKEN, file_path)
-    global img_link
-    img_link = link2
-    logger.info("Picture: %s", img_link)
-
-    mail(gps_location, delivery_number, img_link)
-    update.message.reply_text('A picture of your shipment has been uploaded. Thank you for your cooperation.')
-    return ConversationHandler.END
-
-def location(update: Update, context: CallbackContext):
-    user = update.message.from_user
-    user_location = update.message.location
-    logger.info("Driver ID location %s: (%s, %s)", user.first_name, user_location.latitude, user_location.longitude)
-    global gps_location
-    gps_location = "({}, {})".format(user_location.latitude, user_location.longitude)
-    update.message.reply_text('Your current GPS location has been recorded. Please enter delivery number.')
-    return DN
-=======
->>>>>>> parent of d92b3c4 (Mensaje de bienvenida ES)
-
-
-def delivery_number(update: Update, context: CallbackContext):
-    user = update.message.from_user
-    global delivery_number
-    delivery_number = update.message.text
-    logger.info("Delivery Number: %s", delivery_number)
-    update.message.reply_text('Delivery number is now recorded. Please upload a picture of your shipment.')
-
-    return PHOTO
-
-def cancel(update: Update, context: CallbackContext):
-    user = update.message.from_user
-    logger.info("Driver ID %s decided to cancel location record.", user.first_name, update.message.text)
-
-    update.message.reply_text('Location record has been cancelled.')
-
-    return ConversationHandler.END
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
 
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-
 def main():
-    '''Start Bot'''
-    # Launch Updater
-    updater = Updater("{}".format(TOKEN), use_context=True)
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(TOKEN, use_context=True)
+
     # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
+    dp = updater.dispatcher
 
-    # conversions handled by command
-    dispatcher.add_handler(CommandHandler("start", start))
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
 
-    # conversation handler
-    loc_handler = ConversationHandler(
-        entry_points = [CommandHandler('start_tracking', start_tracking)],
-        states = {
-            LOC: [
-                MessageHandler(Filters.location, location)],
-            PHOTO: [
-                MessageHandler(Filters.photo, photo)],
-            DN:[MessageHandler(Filters.text & ~Filters.command, delivery_number)]
-        },
-        fallbacks = [CommandHandler('cancel', cancel)]
-    )
-    dispatcher.add_handler(loc_handler)
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
 
     # log all errors
-    dispatcher.add_error_handler(error)
-    # Start the Bot
-    updater.start_webhook(listen="0.0.0.0",
-                          port=PORT,
-                          url_path="{}".format(TOKEN))
-    # updater.bot.set_webhook(url=settings.WEBHOOK_URL)
-    updater.bot.set_webhook("{}".format(APP_NAME) + "{}".format(TOKEN))
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.start_polling()
-    updater.idle()
+    dp.add_error_handler(error)
 
+    # Start the Bot
+    updater.start_webhook(
+        listen="0.0.0.0",
+        port=int(PORT),
+        url_path=TOKEN,
+        webhook_url='https://sergesa-bot.herokuapp.com/' + TOKEN
+    )
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
 
 if __name__ == '__main__':
     main()
