7e6101a60cb2724c1be1f143adc3e2c8661ad631
Fernando Zuniga <fernando.zuniga@foodservice.com.gt>
2022-07-23 23:06:55 -0600

    PRUEBA 1

--- bot.py
+++ bot.py
@@ -1,3 +1,9 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.base import MIMEBase
+from email import encoders
 import logging
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext)   
@@ -46,6 +52,41 @@ def photo(update: Update, context: CallbackContext):
     r = requests.get(link1)
     file_path = r.json()["result"]["file_path"]
 
+# Link to download file
+    link2 = "https://api.telegram.org/file/bot{}/{}".format(TOKEN, file_path)
+    global img_link
+    img_link = link2
+    logger.info("Picture: %s", img_link)
+
+    mail(gps_location, delivery_number, img_link)
+    update.message.reply_text('A picture of your shipment has been uploaded. Thank you for your cooperation.')
+    return ConversationHandler.END
+
+def location(update: Update, context: CallbackContext):
+    user = update.message.from_user
+    user_location = update.message.location
+    logger.info("Driver ID location %s: (%s, %s)", user.first_name, user_location.latitude, user_location.longitude)
+    global gps_location
+    gps_location = "({}, {})".format(user_location.latitude, user_location.longitude)
+    update.message.reply_text('Your current GPS location has been recorded. Please enter delivery number.')
+    return DN
+
+def delivery_number(update: Update, context: CallbackContext):
+    user = update.message.from_user
+    global delivery_number
+    delivery_number = update.message.text
+    logger.info("Delivery Number: %s", delivery_number)
+    update.message.reply_text('Delivery number is now recorded. Please upload a picture of your shipment.')
+
+    return PHOTO
+
+def cancel(update: Update, context: CallbackContext):
+    user = update.message.from_user
+    logger.info("Driver ID %s decided to cancel location record.", user.first_name, update.message.text)
+
+    update.message.reply_text('Location record has been cancelled.')
+    
+    return ConversationHandler.END     
 
 
 
@@ -68,18 +109,44 @@ def main():
      # Publicar la versión 12 esto ya no será necesario
     updater = Updater(TOKEN, use_context=True)
 
+# Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # conversions handled by command
+    dispatcher.add_handler(CommandHandler("start", start))
+
+    # conversation handler
+    loc_handler = ConversationHandler(
+        entry_points = [CommandHandler('start_tracking', start_tracking)],
+        states = {
+            LOC: [
+                MessageHandler(Filters.location, location)],
+            PHOTO: [
+                MessageHandler(Filters.photo, photo)],
+            DN:[MessageHandler(Filters.text & ~Filters.command, delivery_number)]
+        },
+        fallbacks = [CommandHandler('cancel', cancel)]
+    )
+    dispatcher.add_handler(loc_handler)
+
+    # log all errors
+    dispatcher.add_error_handler(error)
+
+
+
+
  # Hacer que el despachador registre a los manejadores
-    dp = updater.dispatcher
+   # dp = updater.dispatcher
 
     # en diferentes comandos - respuesta en Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
+    #dp.add_handler(CommandHandler("start", start))
+   # dp.add_handler(CommandHandler("help", help))
 
     # en mensaje sin comando, es decir, repetir el mensaje en Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+   # dp.add_handler(MessageHandler(Filters.text, echo))
 
   # registrar todos los errores
-    dp.add_error_handler(error)
+  #  dp.add_error_handler(error)
 
   # Iniciar el robot
     updater.start_webhook(
@@ -92,6 +159,7 @@ def main():
     # Ejecute el bot hasta que presione Ctrl-C o el proceso reciba SIGINT,
      # SIGTERM o SIGABRT. Esto debe usarse la mayor parte del tiempo, ya que
      # start_polling() no bloquea y detendrá el bot con gracia.
+     
     updater.idle()
 
 if __name__ == '__main__':
